c1c2e5a03b09437d9802cd74f6cf43f01af57d0c
Ashe Austaire <AsheAustaire@gmail.com>
2017-08-26 15:19:49 -0400

    Done.

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -1,65 +1,71 @@
 require 'pry'
 
-def second_supply_for_fourth_of_july(holiday_hash)
-   {
-     :winter => {
-       :christmas => ["Lights", "Wreath"],
-       :new_years => ["Party Hats"]
-     },
-     :summer => {
-       :fourth_of_july => ["Fireworks", "BBQ"]
-     },
-     :fall => {
-       :thanksgiving => ["Turkey"]
-     },
-     :spring => {
-       :memorial_day => ["BBQ"]
-     }
-   }
-   return holiday_hash[:summer][:fourth_of_july][1]
+def second_supply_for_fourth_of_july(hashmap)
+
+    return hashmap[:summer][:fourth_of_july][1]
+
 end
 
-def add_supply_to_winter_holidays(holiday_hash, supply)
-    holiday_hash[:winter].each do |holiday, stuff|
-        stuff << supply
-    end
-    holiday_hash
+#
+
+def add_supply_to_winter_holidays(hashmap, supply)
+
+    hashmap[:winter].collect {|holiday, supplies| hashmap[:winter][holiday] << supply }
+
 end
 
+#
 
-def add_supply_to_memorial_day(holiday_hash, supply)
-    holiday_hash[:spring].each do |holiday, stuff|
-        stuff << supply
-    end
-    holiday_hash
+def add_supply_to_memorial_day(hashmap, supply)
+
+    hashmap[:spring][:memorial_day] << supply
+
 end
 
-def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
-    holiday_hash[season][holiday_name] = supply_array
-    holiday_hash
+#
+
+def add_new_holiday_with_supplies(hashmap, season, holiday_name, supply_array)
+
+    hashmap[season][holiday_name] = supply_array
+
 end
 
+#
 
-def all_winter_holiday_supplies(holiday_hash)
-    holiday_hash[:winter].map do |holiday, supplies|
-        supplies
-    end.flatten
+def all_winter_holiday_supplies(hashmap)
+
+    supplyList = []
+    hashmap[:winter].each {|holiday, supplies| supplyList << supplies }
+    return supplyList.flatten
+
 end
 
-def all_supplies_in_holidays(holiday_hash)
-    holiday_supplies.each do |season, holidays|
-        puts "#{season.capitalize}:"
-        holidays.each do |holiday, supplies|
-            puts "  #{holiday.to_s.split('_').map {|w| w.capitalize }.join(' ') }: #{supplies.join(", ")}"
+#
+
+def all_supplies_in_holidays(hashmap)
+
+    hashmap.each do |season, holiday|
+
+        puts "#{season.to_s.capitalize}:"
+
+        holiday.each do |holiday, supplies|
+
+            puts "  #{holiday.to_s.split('_').map {|word| word.capitalize()}.join(' ')}: #{supplies.join(', ')}"
+
         end
     end
 end
 
-def all_holidays_with_bbq(holiday_hash)
-    holiday_hash.map do |season, holidays|
-        holidays.map do |holiday, supplies|
-            holiday if supplies.include?("BBQ")
-        end
+#
+
+def all_holidays_with_bbq(hashmap)
+
+    newList = []
+
+    hashmap.each do |season, holiday|
+
+        holiday.each {|holiday, supplies| newList << holiday if hashmap[season][holiday].include?('BBQ')}
+
     end
-    .flatten.compact
+    return newList
 end

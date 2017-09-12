a0f9df32e0848994741090c515dbbc98d3e12d65
Anthony Giuliano <agiuliano@gmail.com>
2017-09-12 16:59:21 +0000

    Done.

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -76,20 +76,3 @@ def all_holidays_with_bbq(holiday_hash)
   end
   result
 end
-#
-# hsh = {
-#   :winter => {
-#     :christmas => ["Lights", "Wreath"],
-#     :new_years => ["Party Hats"]
-#   },
-#   :summer => {
-#     :fourth_of_july => ["Fireworks", "BBQ"]
-#   },
-#   :fall => {
-#     :thanksgiving => ["Turkey"]
-#   },
-#   :spring => {
-#     :memorial_day => ["BBQ"]
-#   }
-# }
-# all_holidays_with_bbq(hsh)

00c81a052fdbc0094921e145a11258dbb50127ad
github username <github email address>
2018-03-27 01:55:16 -0400

    Done.

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -18,12 +18,16 @@ def second_supply_for_fourth_of_july(holiday_hash)
   #   }
   # }
   # return the second element in the 4th of July array
+  holiday_hash[:summer][:fourth_of_july][1]
+  
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
   # holiday_hash is identical to the one above
   # add the second argument, which is a supply, to BOTH the
   # Christmas AND the New Year's arrays
+  holiday_hash[:winter][:christmas][2] = supply
+  holiday_hash[:winter][:new_years][1] = supply
 
 end
 
@@ -31,18 +35,21 @@ end
 def add_supply_to_memorial_day(holiday_hash, supply)
   # again, holiday_hash is the same as the ones above
   # add the second argument to the memorial day array
+  holiday_hash[:spring][:memorial_day][1] = supply
+  holiday_hash[:spring][:memorial_day][1] = supply
 
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
   # code here
   # remember to return the updated hash
+  holiday_hash[season][holiday_name] = supply_array
 
 end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
-
+  holiday_hash[:winter].values.flatten
 end
 
 def all_supplies_in_holidays(holiday_hash)
@@ -53,13 +60,41 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
+  holiday_hash.each do |key, values|
+      puts "#{key.to_s.capitalize!}:"
+      #stringKey.capitalize!
+      
+      values.each do |key, array|
+          #binding.pry
+          puts "  #{key.to_s.split('_').collect { |word| word.capitalize }.join(' ')}: #{array.join(', ')}"
+          #puts "#{array.join(', ')}"
+          
+          end
+  end
 
 end
 
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-
+  array = []
+  #binding.pry
+  if holiday_hash[:winter][:christmas].include?("BBQ")
+      array += [:christmas]
+      end
+  if holiday_hash[:winter][:new_years].include?("BBQ")
+      array += [:new_years]
+      end
+  if holiday_hash[:summer][:fourth_of_july].include?("BBQ")
+    array += [:fourth_of_july]
+    end
+  if holiday_hash[:fall][:thanksgiving].include?("BBQ")
+    array += [:thanksgiving]
+    end
+   if holiday_hash[:spring][:memorial_day].include?("BBQ")
+    array += [:memorial_day]
+  end
+  array
 end
 
 

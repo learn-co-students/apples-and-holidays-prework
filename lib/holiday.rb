8bab1f30699fce151cf854b1a99dc2113f40b820
Daniel Walz <danielwalz@Daniels-MacBook-Pro.local>
2018-08-05 16:58:03 -0500

    Done.

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -18,29 +18,45 @@ def second_supply_for_fourth_of_july(holiday_hash)
   #   }
   # }
   # return the second element in the 4th of July array
+  summer = holiday_hash[:summer][:fourth_of_july]
+  summer[1]
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
   # holiday_hash is identical to the one above
   # add the second argument, which is a supply, to BOTH the
   # Christmas AND the New Year's arrays
-
+holiday_hash.each do |season, holiday|
+  if season == :winter
+holiday.each do |holiday, iconic|
+  iconic << supply
+  end
 end
-
+end
+end
 
 def add_supply_to_memorial_day(holiday_hash, supply)
   # again, holiday_hash is the same as the ones above
   # add the second argument to the memorial day array
-
+  holiday_hash.each do |season, holiday|
+    if season == :spring
+  holiday.each do |holiday, iconic|
+    iconic << supply
+    end
+  end
+  end
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
   # code here
+  holiday_hash[season][holiday_name] =supply_array
   # remember to return the updated hash
+  holiday_hash
 
 end
 
 def all_winter_holiday_supplies(holiday_hash)
+  holiday_hash[:winter].values.flatten
   # return an array of all of the supplies that are used in the winter season
 
 end
@@ -53,18 +69,20 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-
+  holiday_hash.each do |season, holidays|
+    puts "#{season.capitalize}:"
+    holidays.each do |holiday, iconic|
+puts"  #{holiday.to_s.split('_').map {|w| w.capitalize }.join(' ')}: #{iconic.join(", ")}"
+end
+end
 end
 
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-
+  holiday_hash.collect do |season,holidays|
+    holidays.collect do |holiday, iconic|
+      holiday if iconic.include?("BBQ")
+    end
+end.flatten.compact
 end
-
-
-
-
-
-
-

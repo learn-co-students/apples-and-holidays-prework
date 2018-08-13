fc9deddc68cbd5318449a395410711d443195310
Omar Gonzalez <ogonzaleznyc@gmail.com>
2018-08-13 00:24:03 +0000

    Done.

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -22,9 +22,6 @@ def second_supply_for_fourth_of_july(holiday_hash)
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
-  # holiday_hash is identical to the one above
-  # add the second argument, which is a supply, to BOTH the
-  # Christmas AND the New Year's arrays
   holiday_hash.each do |season, hash_of_seasonal_holidays|
     if season == :winter 
       hash_of_seasonal_holidays.each do |holiday, array_of_supplies|
@@ -33,11 +30,7 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
         hash_of_seasonal_holidays
     end
     :winter 
-    
   end 
-  
-  
-
 end
 
 

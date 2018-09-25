ee9c8960b8614ca3153cd4becb674b2120a24857
Cara Morelli <caramorelli7@gmail.com>
2018-09-25 03:22:31 +0000

    Done.

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -18,30 +18,40 @@ def second_supply_for_fourth_of_july(holiday_hash)
   #   }
   # }
   # return the second element in the 4th of July array
+  
+  holiday_hash[:summer][:fourth_of_july][1]
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
   # holiday_hash is identical to the one above
   # add the second argument, which is a supply, to BOTH the
   # Christmas AND the New Year's arrays
-
+  holiday_hash[:winter].each do |key, val|
+    val << supply
+  end
+  holiday_hash
 end
 
 
 def add_supply_to_memorial_day(holiday_hash, supply)
   # again, holiday_hash is the same as the ones above
   # add the second argument to the memorial day array
-
+  holiday_hash[:spring][:memorial_day] << supply 
+  holiday_hash
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
   # code here
   # remember to return the updated hash
+  holiday_hash[season][holiday_name] = supply_array
+  holiday_hash
+  
 
 end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
+  holiday_hash[:winter].values.flatten
 
 end
 
@@ -53,17 +63,39 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-
+  
+  holiday_hash.each do |key, val|
+    puts key.to_s.capitalize! + ':'
+    val.each do |k, v|
+      new_k = ''
+      k.to_s.chars.each do |char| 
+        if char != '_'  
+          new_k << char
+        else 
+          new_k << ' '
+        end 
+      end 
+      puts '  ' + (new_k.split(' ').each {|word| word.capitalize!}).join(' ') + ': ' + v.join(', ')
+    end 
+  end 
+      
 end
+
+
+
 
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-
+  bbq_arr = []
+  holiday_hash.each do |val, key|
+    key.each do |k, y|
+      bbq_arr << k if y.include?("BBQ")
+    end 
+    
+  end 
+  bbq_arr
 end
-
-
-
 
 
 

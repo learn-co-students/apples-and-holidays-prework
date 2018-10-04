77cecc526c285889e48c72764586baad431022b0
Jacob Menashi <jbmenashi@gmail.com>
2018-10-04 17:11:05 +0000

    Done.

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -38,31 +38,36 @@ def add_supply_to_memorial_day(holiday_hash, supply)
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
-  holiday_hash[season] = holiday_name
-  holiday_hash[season][holiday_name] = "Bob"
-  binding.pry
+  holiday_hash[season][holiday_name] = supply_array
   holiday_hash
 end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
-
+  holiday_hash[:winter].values.flatten
 end
 
 def all_supplies_in_holidays(holiday_hash)
-  # iterate through holiday_hash and print items such that your readout resembles:
-  # Winter:
-  #   Christmas: Lights, Wreath
-  #   New Years: Party Hats
-  # Summer:
-  #   Fourth Of July: Fireworks, BBQ
-  # etc.
-
+  holiday_hash.each do |season, holiday|
+    puts "#{season.capitalize}:"
+    holiday.each do |holiday, supply|
+      puts "  #{holiday.to_s.split("_").map {|each_word| each_word.capitalize}.join(" ")}: #{supply.join(", ")}"
+    end
+  end
 end
 
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
+  new_array = []
+  holiday_hash.each do |season, holiday|
+    holiday.each do |holiday, supplies|
+      if supplies.include?("BBQ")
+        new_array << holiday
+      end
+    end
+  end
+  new_array
 
 end
 

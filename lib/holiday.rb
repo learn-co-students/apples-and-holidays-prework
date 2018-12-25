1e145514d15d4287c17de7628e148ce5c5dbc856
mkay0961 <mkay0961@gmail.com>
2018-12-25 18:16:50 -0500

    Done.

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -18,12 +18,19 @@ def second_supply_for_fourth_of_july(holiday_hash)
   #   }
   # }
   # return the second element in the 4th of July array
+
+  return holiday_hash[:summer][:fourth_of_july][1]
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
   # holiday_hash is identical to the one above
   # add the second argument, which is a supply, to BOTH the
   # Christmas AND the New Year's arrays
+
+  holiday_hash[:winter].each do |k,v|
+    holiday_hash[:winter][k] << supply
+  end
+
 
 end
 
@@ -32,16 +39,29 @@ def add_supply_to_memorial_day(holiday_hash, supply)
   # again, holiday_hash is the same as the ones above
   # add the second argument to the memorial day array
 
+  holiday_hash[:spring].each do |k,v|
+    holiday_hash[:spring][k] << supply
+  end
+
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
   # code here
   # remember to return the updated hash
-
+  hash1 = {holiday_name => supply_array }
+  holiday_hash[season] = hash1
+  return holiday_hash
 end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
+  array =[]
+  holiday_hash[:winter].each do |k, v|
+    array.push(v)
+
+  end
+
+  return array.flatten
 
 end
 
@@ -54,17 +74,34 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
 
+  holiday_hash.each do |k, v|
+    puts "#{k.capitalize}:"
+    holiday_hash[k].each do | k, v|
+      leter = "#{k}"
+      leter = leter.tr("_", " ")
+      leter = leter.split.map(&:capitalize).join(' ')
+
+      puts "  #{leter}: #{v.join(", ")}"
+
+    end
+
+  end
+
+
 end
 
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
+  array = []
+  holiday_hash.each do |k, v|
+    holiday_hash[k].each do | k, v|
+    if v.include?("BBQ")
+        array.push(k)
+    end
+    end
+
+  end
+  return array
 
 end
-
-
-
-
-
-
-
